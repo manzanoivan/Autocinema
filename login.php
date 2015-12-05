7b5be1d702d59a16f4f25c8a6ac238edb4d26c95
manzanoivan <ivan_ajedrez@hotmail.com>
2015-12-05 15:47:59 -0600

    Cambios a login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     require_once("Usuario.php");
     require_once ("Menu.php");
     session_start();
-    if( @!is_null( $_SESSION["id"] ) ){
+    if(@!is_null( $_SESSION["Usuario"] ) ){
         $usuario = $_SESSION["Usuario"];
     }  
 ?>
@@ -12,9 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <title>Autocinema Coyote</title>
-
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
@@ -47,39 +45,51 @@
         ?>
         
 
-	<section id="login" class="home-section">
-		  <div class="container card-login">
-      
-          <form class="form-login" action="loginAction.php" method="POST">
-            <h2 class="form-login-heading">Login</h2>
-            <div class="login-wrap">
-                <input class="form-control" placeholder="Usuario" autofocus="" type="text" name="user">
-                <br>
-                <input class="form-control" placeholder="Password" type="password" name="password">
-                <label class="checkbox">
-                    <span class="pull-right">
-                        <!--espacio entre iniciar y campos -->
-                    </span>
-                </label>
-                <button class="btn btn-theme btn-block" href="index.html" type="submit"><i class="fa fa-lock"></i>Iniciar Sesión</button>
-                <hr>
-                
-                <div class="login-social-link centered">
-                <p>Mediante Redes Sociales</p>
-                    <button class="btn btn-facebook" type="submit"><i class="fa fa-facebook"></i> Facebook</button>
-                    <button class="btn btn-twitter" type="submit"><i class="fa fa-twitter"></i> Twitter</button>
+    <section id="login" class="home-section">
+        <div class="container card-login">
+            <?php
+            if( @is_null( $usuario ) || @is_null($usuario->getId()) ){
+                echo setMenu(-1);
+            
+            ?>
+            <form class="form-login" action="loginAction.php" method="POST">
+                <h2 class="form-login-heading">Login</h2>
+                <div class="login-wrap">
+                    <input class="form-control" placeholder="Usuario" autofocus="" type="text" name="user">
+                    <br>
+                    <input class="form-control" placeholder="Password" type="password" name="password">
+                    <label class="checkbox">
+                        <span class="pull-right">
+                            <!--espacio entre iniciar y campos -->
+                        </span>
+                    </label>
+                    <button class="btn btn-theme btn-block" href="index.html" type="submit"><i class="fa fa-lock"></i>Iniciar Sesión</button>
+                    <hr>                
+                    <div class="login-social-link centered">
+                        <p>Mediante Redes Sociales</p>
+                        <button class="btn btn-facebook" type="submit"><i class="fa fa-facebook"></i> Facebook</button>
+                        <button class="btn btn-twitter" type="submit"><i class="fa fa-twitter"></i> Twitter</button>
+                    </div>
+                    <div class="registration">
+                        Aún no estas registrado?<br>
+                        <a class="" href="#">
+                            Crear una cuenta
+                        </a>
+                    </div>    
                 </div>
-                <div class="registration">
-                    Aún no estas registrado?<br>
-                    <a class="" href="#">
-                        Crear una cuenta
-                    </a>
-                </div>
-    
-            </div>
-          </form>     
-      </div>
-  </section>
+            </form>  
+            <?php
+            }
+            else{
+            ?>    
+                <form class="form-login" action="loginAction.php" method="POST">
+                    <h2 class="form-login-heading">Ya has iniciado sesión</h2>
+                </form>
+            <?php
+            }
+            ?>
+        </div>
+    </section>
 
     <!-- Core JavaScript Files -->
     <script src="js/jquery.min.js"></script>
--- signin.php
+++ signin.php
@@ -1,3 +1,11 @@
+<?php
+    require_once("Usuario.php");
+    require_once ("Menu.php");
+    session_start();
+    if(@!is_null( $_SESSION["Usuario"] ) ){
+        $usuario = $_SESSION["Usuario"];
+    }  
+?>
 <!DOCTYPE html>
 <html>
 
@@ -29,39 +37,14 @@
 	  <div id="load"></div>
 	</div>
 
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.html">
-                    <h1>AutoCinema</h1>
-                </a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-     <ul class="nav navbar-nav">
-        <li class="active"><a href="index.html#intro">Home</a></li>
-        <li><a href="content-filter.html">Cartelera</a></li>
-		<li><a href="index.html#ubicacion">Ubicación</a></li>
-		<li><a href="index.html#contacto">Contacto</a></li>
-		<li><a href="product-comparison.html">Cafeteria</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="login.html">Iniciar Sesión</a></li>
-            <li><a href="signin.html">Registrarse</a></li>
-          </ul>
-		
-        </li>
-      </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+        <?php
+            if( @is_null( $usuario ) || @is_null($usuario->getId()) ){
+                echo setMenu(-1);
+            }
+            else{
+                echo setMenu( $usuario->getTipo() );
+            }
+        ?>
 
 	<section id="login" class="home-section">
 		  <div class="container card-login">
