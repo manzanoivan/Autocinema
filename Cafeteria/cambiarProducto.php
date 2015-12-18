ac6d981f43ad2d746bf671cdac19aeee975d1bb4
manzanoivan <ivan_ajedrez@hotmail.com>
2015-12-18 00:03:18 -0600

    fix cambiar producto

--- Cafeteria/cambiarProducto.php
+++ Cafeteria/cambiarProducto.php
@@ -16,11 +16,11 @@
 		if( $_FILES['image']['tmp_name'] != "" ){	
 			echo "NO ES NULL";
 			$image = addslashes(file_get_contents($_FILES['image']['tmp_name'])); 
-			$query = "UPDATE productocafeteria SET nombre = '{$nombre}', descripcion = '{$descripcion}', precio = '{$precio}', imagen = '{$image}' WHERE idProducto = '{$id}'";
+			$query = "UPDATE productoCafeteria SET nombre = '{$nombre}', descripcion = '{$descripcion}', precio = '{$precio}', imagen = '{$image}' WHERE idProducto = '{$id}'";
 		}
 		else{
 			echo "ES NULL";
-			$query = "UPDATE productocafeteria SET nombre = '{$nombre}', descripcion = '{$descripcion}', precio = '{$precio}' WHERE idProducto = '{$id}'";
+			$query = "UPDATE productoCafeteria SET nombre = '{$nombre}', descripcion = '{$descripcion}', precio = '{$precio}' WHERE idProducto = '{$id}'";
 		}
 		$link = conecta();
 		mysqli_set_charset($link, "utf8");
